8894af142c8ab09da9f205046f7b95c239e6d61b
Moonhyuk Choi <moonchoi@microsoft.com>
2021-10-12 16:35:39 +0900

    UCASLPane

---
+++ UCASLPane.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace SignLanguageAssistant
+{
+	partial class UCASLPane
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCASLPane));
+			System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Slide1",
+            "completed",
+            "aaa"}, 0);
+			System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Slide2",
+            "need notepage"}, -1);
+			this.btnVoiceRec = new System.Windows.Forms.CheckBox();
+			this.txtResult = new System.Windows.Forms.TextBox();
+			this.btnPlayTest = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnVoiceRec
+			// 
+			this.btnVoiceRec.Appearance = System.Windows.Forms.Appearance.Button;
+			this.btnVoiceRec.AutoSize = true;
+			this.btnVoiceRec.Location = new System.Drawing.Point(0, 225);
+			this.btnVoiceRec.Name = "btnVoiceRec";
+			this.btnVoiceRec.Size = new System.Drawing.Size(129, 22);
+			this.btnVoiceRec.TabIndex = 2;
+			this.btnVoiceRec.Text = "voice recognize test";
+			this.btnVoiceRec.UseVisualStyleBackColor = true;
+			this.btnVoiceRec.CheckedChanged += new System.EventHandler(this.btnVoiceRec_CheckedChanged);
+			// 
+			// txtResult
+			// 
+			this.txtResult.Location = new System.Drawing.Point(0, 253);
+			this.txtResult.Multiline = true;
+			this.txtResult.Name = "txtResult";
+			this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+			this.txtResult.Size = new System.Drawing.Size(197, 163);
+			this.txtResult.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(3, 3);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(128, 23);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Process All";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(3, 32);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(189, 10);
+			this.progressBar1.TabIndex = 7;
+			this.progressBar1.Value = 30;
+			// 
+			// listView1
+			// 
+			this.listView1.BackColor = System.Drawing.SystemColors.Control;
+			this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+			this.listView1.HideSelection = false;
+			this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem3,
+            listViewItem4});
+			this.listView1.Location = new System.Drawing.Point(3, 48);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(221, 116);
+			this.listView1.SmallImageList = this.imageList1;
+			this.listView1.TabIndex = 9;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Slide No.";
+			this.columnHeader1.Width = 79;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Status";
+			this.columnHeader2.Width = 100;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "icons8-check-64.png");
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(135, 225);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 10;
+			this.button2.Text = "video test";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// UCASLPane
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.btnPlayTest);
+			this.Controls.Add(this.txtResult);
+			this.Controls.Add(this.btnVoiceRec);
+			this.Name = "UCASLPane";
+			this.Size = new System.Drawing.Size(246, 623);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.CheckBox btnVoiceRec;
+		private System.Windows.Forms.TextBox txtResult;
+		private System.Windows.Forms.Button btnPlayTest;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ UCASLPane.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+
+namespace SignLanguageAssistant
+{
+	public partial class UCASLPane : UserControl
+	{
+
+        SpeechConfig config = SpeechConfig.FromSubscription("dafa0b166eb24ced8ac4e606336701e1", "southeastasia");
+
+		public UCASLPane()
+		{
+			InitializeComponent();
+        }
+
+     
+
+
+        private void btnVoiceRec_CheckedChanged(object sender, EventArgs e)
+		{
+			if (btnVoiceRec.Checked)
+			{
+                Task.Run(async () => await RecognitionWithMic());
+                //RecognitionWithMic().GetAwaiter().GetResult();
+
+            }
+			else
+			{
+                //Task.Run(async () => await  recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false))
+			}
+		}
+
+		private async Task RecognitionWithMic()
+		{
+            var stopRecognition = new TaskCompletionSource<int>();
+
+            using (var recognizer = new SpeechRecognizer(config))
+			{
+
+                //var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+                // Subscribes to events.
+                recognizer.Recognizing += (s, e) =>
+                {
+                    if (e.Result.Reason == ResultReason.RecognizingSpeech)
+                    {
+                        txtResult.Invoke(new MethodInvoker(() => txtResult.Text += "\r\nRECOGNIZING: " + e.Result.Text + "\r\n"));
+                        
+                        //txtResult.Invoke(new MethodInvoker(() =>
+                        //{
+                        //    txtResult.SelectionStart = txtResult.Text.Length;
+                        //    txtResult.ScrollToCaret();
+                        //}));
+                        
+
+                        //Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                        //// Retrieve the detected language
+                        //var autoDetectSourceLanguageResult = AutoDetectSourceLanguageResult.FromResult(e.Result);
+                        //Console.WriteLine($"DETECTED: Language={autoDetectSourceLanguageResult.Language}");
+                    }
+                };
+
+                recognizer.Recognized += (s, e) =>
+                {
+                    if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                    {
+
+                        txtResult.Invoke(new MethodInvoker(() => txtResult.Text += "\r\nRECOGNIZED: " + e.Result.Text));
+                        txtResult.Invoke(new MethodInvoker(() => txtResult.Text += "\r\n-------------------- "));
+                        txtResult.Invoke(new MethodInvoker(() => txtResult.Text += "\n"));
+                        txtResult.Invoke(new MethodInvoker(() =>
+                        {
+                            txtResult.SelectionStart = txtResult.Text.Length;
+                            txtResult.ScrollToCaret();
+                        }));
+
+                        //Console.WriteLine($"RECOGNIZED: Text={e.Result.Text}");
+                        //// Retrieve the detected language
+                        //var autoDetectSourceLanguageResult = AutoDetectSourceLanguageResult.FromResult(e.Result);
+                        //Console.WriteLine($"DETECTED: Language={autoDetectSourceLanguageResult.Language}");
+                    }
+                    else if (e.Result.Reason == ResultReason.NoMatch)
+                    {
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    }
+                };
+
+                recognizer.Canceled += (s, e) =>
+                {
+                    Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                    if (e.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={e.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+
+                    stopRecognition.TrySetResult(0);
+                };
+
+                recognizer.SessionStarted += (s, e) =>
+                {
+                    txtResult.Invoke(new MethodInvoker(() => txtResult.Text = "\n    Session started event."));
+
+                
+
+                    Console.WriteLine("\n    Session started event.");
+                };
+
+                recognizer.SessionStopped += (s, e) =>
+                {
+                    Console.WriteLine("\n    Session stopped event.");
+                    Console.WriteLine("\nStop recognition.");
+                    stopRecognition.TrySetResult(0);
+                };
+
+               await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                // Waits for completion.
+                // Use Task.WaitAny to keep the task rooted.
+                Task.WaitAny(new[] { stopRecognition.Task });
+
+                // Stops recognition.
+                await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+
+            }
+		}
+
+	
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+            PowerPoint.Application app = Globals.ThisAddIn.Application;
+            PowerPoint.SlideRange slideRange = app.ActiveWindow.Selection.SlideRange;
+
+            var presentation = app.ActivePresentation;
+
+
+            var slide = presentation.Slides[slideRange.SlideNumber];
+            var with = slide.Application.Width;
+            var height = slide.Application.Height;
+            slide.Shapes.AddMediaObject2(@"C:\Project\ASL\ref\Speech-to-ASL\output.mp4", Left:with-530, Top:height-230, Width:300, Height:200);
+            
+        }
+	}
+}
