05fe2971539c4647f7214844cfb3b61104501a22
Moonhyuk Choi <moonchoi@microsoft.com>
2021-10-12 16:30:31 +0900

    initial Commit

---
+++ Ribbon1.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace SignLanguageAssistant
+{
+	public partial class Ribbon1
+	{
+		private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+		{
+			//TODO: implement custom tooltip with container(HearMeSeeMe in Action v2.0.pptx, page 15)
+			this.btnSL.SuperTip = "Click to Turn on Sign Language Assistant\r\nBe Understood. Make this presentation Accessible";
+
+		}
+
+
+		private void btnSL_Click(object sender, RibbonControlEventArgs e)
+		{
+			if (!btnSL.Checked)
+			{
+				Globals.ThisAddIn.myCustomTaskPane.Visible = false;
+				return;
+			}
+
+			//show taskpane
+			Globals.ThisAddIn.myCustomTaskPane.Visible = true;
+
+			PowerPoint.Application app = Globals.ThisAddIn.Application;
+			PowerPoint.SlideRange slideRange = app.ActiveWindow.Selection.SlideRange;
+
+			var presentation = app.ActivePresentation;
+
+
+			var slide = presentation.Slides[slideRange.SlideNumber];
+
+			// https://www.free-power-point-templates.com/articles/create-powerpoint-ppt-programmatically-using-c/
+			// wtf!! note index fixed.
+			var note = slide.NotesPage.Shapes[2].TextFrame.TextRange.Text;
+
+
+			// parse text only ===HearMeSeeMe=== section
+			if (string.IsNullOrEmpty(note))
+			{
+				//System.Windows.Forms.MessageBox.Show("note emply");
+			}
+			else
+			{
+				/*
+				
+				test script in notepage
+
+				===HearMeSeeMe===
+				hi, there
+				There over are 1 billion people in the world who have disabilities, many of whom need assistive technology. But only 1 in 10 have access to the products needed, and this means many of them can't fully participate in our economies and societies. Accessible technology can allow them to fully participate. 
+
+				===HearMeSeeMe===
+
+				 */
+
+				//split line by line
+				var notes = note.Split("\r".ToCharArray());
+
+				const string SECTION = "===HearMeSeeMe===";
+				int intSectionCnt = 0;
+
+				var parsedNotes = string.Empty;
+
+				foreach (var line in notes)
+				{
+					if (line.Contains(SECTION))
+					{
+						intSectionCnt++;
+						continue;
+					}
+
+					if (intSectionCnt == 0 || intSectionCnt == 2) continue;
+
+					parsedNotes += line + "\r";
+				}
+				System.Windows.Forms.MessageBox.Show(parsedNotes);
+			}
+		}
+	}
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace SignLanguageAssistant
+{
+    
+    public partial class ThisAddIn
+    {
+        public UCASLPane myUCASLPane;
+        public Microsoft.Office.Tools.CustomTaskPane myCustomTaskPane;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            myUCASLPane = new UCASLPane();
+            myCustomTaskPane = this.CustomTaskPanes.Add(myUCASLPane, "SL");
+            myCustomTaskPane.Visible = false;
+
+            //TODO: research how to add control to powerpoint status bar.
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
